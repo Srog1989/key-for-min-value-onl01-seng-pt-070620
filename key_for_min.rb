43ae47d41985030f3e7bb237f5e949e60fa6c2fd
Sarah Rogers <s.rogers0715@yahoo.com>
2020-07-18 12:43:52 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,7 +7,7 @@ def key_for_min_value(name_hash)
   name_hash.each do |key, value|
    if lowest_value == nil || value< lowest_value
      lowest_key= key
-     lowest_key= key 
+     lowest_value= value
    end
   end
   lowest_key
